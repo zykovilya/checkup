7727f7dd5bba1025fa874593d7cc219f0086fd56
comrade <izykov@drclinics.ru>
2019-11-19 13:35:10 +0300

    questions fix

--- lua/questions.lua
+++ lua/questions.lua
@@ -3,4 +3,5 @@ local body = ngx.req.get_body_data()
 local file = io.open(string.format("/usr/share/nginx/html/files/questions/%s",os.clock()), 'w')
 file:write(body)
 file:close()
+return ngx.redirect("https://test-telemed.drclinics.ru/msa/api/questions/success.html",301)
 
