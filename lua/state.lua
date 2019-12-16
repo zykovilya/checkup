77e1c2be8ad1ae38f5850229a4e78c7c6f02083e
VadimZR <volkovvadimrussia@gmail.com>
2019-12-16 14:22:19 +0300

    Revert "Merge remote-tracking branch 'upstream/vadim'"
    This reverts commit 14ec1a648c1d4f75da40b22b0826994bb39a3750, reversing
    changes made to 2176006f527ecaeb7a327d9a35113782af134afb.

--- lua/state.lua
+++ lua/state.lua
@@ -1,47 +1,27 @@
-local cjson = require "cjson"
-local auth = require "lua.modules.telemed_auth"
-local utils = require "lua.modules.utils"
 
-
-local personId  = ngx.var.personId
-local productOrderId  = ngx.var.productOrderId
-
-
-local patientInfo = auth.patientInfo(ngx,os.getenv("TMP_SERVER_URL").."/api/auth/person",personId)
-
-ngx.header["firstName"] = patientInfo.firstName
-
-
-local function getFileName(dirName, personId,productOrderId)
-    return string.format(dirName .. "%s_%s.json",personId,productOrderId)
+function getFileName()
+    local patientId  = ngx.var.patientId
+    local clientProductId  = ngx.var.clientProductId
+    local outFile = string.format("/usr/share/nginx/html/files/state/%s_%s.json",patientId,clientProductId)
+    return outFile
 end
 
-local dirName = '/files/state/'
-utils.checkAndCreateDirs(dirName);
+local cjson = require "cjson"
 
 if ngx.req.get_method() == "POST" or ngx.req.get_method() == "PUT"  then
 
     ngx.req.read_body()
-    local body = ngx.req.get_body_data()
-    utils.checkNotNull(body, 'Required request body content is missing');
-    body = cjson.decode(body)
+    local body = cjson.decode(ngx.req.get_body_data())
 
-    local file = io.open(getFileName(dirName, personId,productOrderId), 'w')
+    local file = io.open(getFileName(), 'w')
     file:write(cjson.encode(body))
     file:close()
     return 200
 else
-    local stateFile = getFileName(dirName, personId, productOrderId);
-    if utils.fileExists(stateFile) then
-        local file = io.open(stateFile);
-        local body = file:read("*all");
-        ngx.say(body);
-        file:close()
-    else
-        ngx.status = 404
-        ngx.print(utils.getErrorResponse("NOT_FOUND","state not found"))
-        return ngx.exit(404)
-    end
+    local file = io.open(getFileName(), 'r')
+    local body = file:read()
+    ngx.say(body)
+    file:close()
 end
 
 
