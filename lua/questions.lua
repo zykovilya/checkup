ded9e49c68a9c79ca1f86e7e22acec3d62c1e972
comrade <izykov@drclinics.ru>
2019-12-26 14:04:01 +0300

    временное прикрепление анкеты как LAB_RESULT

--- lua/questions.lua
+++ lua/questions.lua
@@ -63,7 +63,7 @@ local file = io.open(filePath, 'w')
 file:write(csv)
 file:close()
 ----------------
-local attachId= attacher.attachFileToPatient(tmpServer, ngx.req.get_headers()["cookie"],filePath, "QUESTIONNAIRE")
+local attachId= attacher.attachFileToPatient(tmpServer, ngx.req.get_headers()["cookie"],filePath, "LAB_RESULT")
 if(attachId~=nil and attachId~="") then
     utils.log("WITH ORDER "..productOrderId .. " [ATTACH_ID]= ".. cjson.encode(attachId))
 else
